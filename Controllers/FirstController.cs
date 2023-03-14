913e8d95306851743788a961370fbb2eb850d1a6
cvetkoskateodora <tc28509@seeu.edu.mk>
2023-03-14 13:43:25 +0100

    commit all

---
+++ Controllers/FirstController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectSOA.Controllers
+{
+    public class FirstController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+
+[ApiController]
+[Route("[controller]")]
+public class  FirstController : Controller
+{
+    [HttpGet]
+    public ActionResult<string> Get()
+    {
+        return "Helo, World!";
+    }
+}
